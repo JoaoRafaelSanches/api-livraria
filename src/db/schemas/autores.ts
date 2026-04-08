75592477a8ce97b33e699160f37db18cfa4b8da1
João Rafael Sanches <jsanchesedantas@gmail.com>
2026-04-07 22:34:15 -0300

    pegar o arquivo drizzle com o midiel

--- src/db/schemas/autores.ts
+++ src/db/schemas/autores.ts
@@ -1,7 +1,10 @@
 import { int, varchar, mssqlTable } from 'drizzle-orm/mssql-core';
 
-export const autores = mssqlTable('autores', {
+export const autoresTabela = mssqlTable('autores', {
   id: int('id').primaryKey().identity(),
   nome: varchar('nome', { length: 100 }).notNull(),
   email: varchar('email', { length: 255 }).notNull().unique(),
 });
+
+export type Autor = typeof autoresTabela.$inferSelect;
+export type CriarAutor = typeof autoresTabela.$inferInsert;
